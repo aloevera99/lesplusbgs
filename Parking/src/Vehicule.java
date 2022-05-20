4c87fdf6f99765554630e4795ead3cdb075e786f
Clément Le Batard <clement.lebatard1@gmail.com>
2022-05-20 17:08:56 +0200

    creation d'une voiture

--- Parking/src/Vehicule.java
+++ Parking/src/Vehicule.java
@@ -1,2 +1,3 @@
 public class Vehicule {
+    public int nb_roues;
 }
--- Parking/src/Voiture.java
+++ Parking/src/Voiture.java
@@ -1,5 +1,11 @@
 public class Voiture extends Vehicule{
     public String marque;
     public String couleur;
-    
+
+    void voiture(String marque, String couleur){
+        this.couleur = couleur;
+        this.marque = marque;
+        super.nb_roues = 4;
+    }
+
 }
