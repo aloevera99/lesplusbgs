b4eaf5f5f0c0e803d4aab77f9486bfd08ebdc18e
Clément Le Batard <clement.lebatard1@gmail.com>
2022-05-20 17:40:52 +0200

    Truc de fou dans les deux seules classes

--- Parking/src/Vehicule.java
+++ Parking/src/Vehicule.java
@@ -1,3 +1,15 @@
 public class Vehicule {
     public int nb_roues;
+
+    public static void main(String[] args) {
+
+        Voiture voiture1 = new Voiture("Dacia", "Bleu",true);
+        System.out.println("Voiture 1 : " + voiture1.demarrer());
+        System.out.println("Voiture 1 : " + voiture1.arret());
+
+        Voiture voiture2 = new Voiture("Lotus", "Verte", false);
+        System.out.println("Voiture 2 : " + voiture2.demarrer());
+
+
+    }
 }
--- Parking/src/Voiture.java
+++ Parking/src/Voiture.java
@@ -1,11 +1,26 @@
 public class Voiture extends Vehicule{
     public String marque;
     public String couleur;
+    public Boolean sportive;
 
-    void voiture(String marque, String couleur){
+    public Voiture(String marque, String couleur, Boolean sportive){
         this.couleur = couleur;
         this.marque = marque;
+        this.sportive = sportive;
         super.nb_roues = 4;
+
+        if(sportive == true){
+            System.out.println("Je suis une voiture sportive ! ");
+        }else
+            System.out.println("Je ne suis pas une voiture sportive ! ");
+    }
+
+    public String demarrer(){
+        return  "Je demarre la voiture";
+    }
+
+    public String arret(){
+        return "Je m'arrete";
     }
 
 }
